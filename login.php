0823abfc615320ba39dd2c0a2a3366c183b3c6fe
LucileV <lverrecchia@gmail.com>
2018-01-15 10:14:14 +0100

    formulaire de contact

--- login.php
+++ login.php
@@ -1,31 +1,77 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Formulaire de connexion</title>
-	<link rel="stylesheet" href="">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-</head>
-<body>
-	<div  class="container">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <style>
+
+    	.container {
+    		padding: 50px;
+    		background-color: AliceBlue;
+    		margin-top: 100px;
+    		-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+			-moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+			box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+    	}
+
+    </style>
+
+    <title>Formulaire de connexion</title>
+  </head>
+  <body>
+
+    <div  class="container">
 	<h1>Formulaire de connexion</h1>
-	<form>
-	  <div class="form-group">
-	    <label for="exampleInputEmail1">Email address</label>
-	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-	    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-	  </div>
-	  <div class="form-group">
-	    <label for="exampleInputPassword1">Password</label>
-	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-	  </div>
-	  <div class="form-check">
-	    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-	    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-	  </div>
-	  <button type="submit" class="btn btn-primary">Submit</button>
-	</form>
+		
+		<form class="">
+			<div class="row">
+			  <div class="col-6">
+			    <label for="nom">Nom</label>
+			    <input type="nom" class="form-control" id="exampleInputEmail1" aria-describedby="nom" placeholder="Nom">  
+			  </div>
+			  <div class="col-6">
+			    <label for="prenom">Prénom</label>
+			    <input type="prenom" class="form-control" id="exampleInputEmail1" aria-describedby="nom" placeholder="Prénom">  
+			  </div>
+			   <div class="col-6">
+			    <label for="pseudo">Pseudo</label>
+			    <input type="pseudo" class="form-control" id="exampleInputEmail1" aria-describedby="nom" placeholder="Pseudo">  
+			  </div>
+			  <div class="col-6" >
+			    <label for="password">Password</label>
+			    <input type="password" class="form-control" id="password" placeholder="Password">
+			  </div>
+			<div class="col-12">
+				<label for="email">Email address</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			</div>
+			<div class="form-check col-6">
+			    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+			    <label class="form-check-label" for="exampleCheck1"> J'ai lu les conditions</label>
+			  </div>
+			  <div class="col-6">
+			  	  <button type="submit" class="btn btn-info">Envoyer le formulaire</button>
+			  </div>
+			
+		  	</div>
+		</form>
+
 	</div>
-</body>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
 </html>
+
